ce201524b4eed5182cd0883b0a4deb8a8003c643
Rustam <drbtrmr@gmail.com>
2024-06-27 18:40:33 +0500

    add pageup arrow

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,4 +42,21 @@ $(document).ready(function() {
             header.removeClass("header__nav_fixed");
         }
     }
+
+    // Smooth scroll and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1500) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
