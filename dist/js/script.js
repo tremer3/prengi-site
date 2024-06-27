1b8476da0e19e242cca9952853ad50dce7c7da1e
Rustam <drbtrmr@gmail.com>
2024-06-27 21:24:24 +0500

    add slick-slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -59,4 +59,20 @@ $(document).ready(function() {
     });
 
     new WOW().init();
+
+    $('.solutions__item').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+              },
+        ]
+    });
 });
