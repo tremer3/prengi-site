80572dc0acebe8761b7a71a9b312dda87dca9518
Rustam <drbtrmr@gmail.com>
2024-06-27 18:21:10 +0500

    add fixed header and hamburger

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const header__nav = document.querySelector('.header__nav'),
+    headerItem = document.querySelectorAll('.header__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        header__nav.classList.toggle('header__nav_active');
+    });
+
+    headerItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            header__nav.classList.toggle('header__nav_active');
+        })
+    })
+})
+
+$(document).ready(function() {
+    
+    /* Fixed Header */ 
+    let header = $(".header__nav");
+    let promo = $(".promo");
+    let promoH = promo.innerHeight();;
+    let scrollPos = $(window).scrollTop();
+    let nav = $(".header__nav");
+    let navToggle = $(".header__navToggle");
+
+    checkScroll(scrollPos, promoH)
+
+    $(window).on("scroll resize", function() {
+        promoH = promo.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, promoH);
+    });
+
+    function checkScroll(scrollPos, promoH) {
+        if( scrollPos > promoH ) {
+            header.addClass("header__nav_fixed");
+        } else {
+            header.removeClass("header__nav_fixed");
+        }
+    }
+});
